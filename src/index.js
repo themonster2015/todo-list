36693a1f512b68458627ffb93602dd43095755c2
themonster2015 <quynhyen.vothi@gmail.com>
2021-08-17 15:36:10 +0200

    created basic todo list interface

--- src/index.js
+++ src/index.js
@@ -1,13 +1,57 @@
-import _ from "lodash"
 import './style.css'
+let todos = [{
+  description: "wash the dish",
+  completed: false,
+  index: 1
+},
+{
+  description: "complete the to do list project",
+  completed: false,
+  index: 2
+},
+{
+  description: "feed the dog",
+  completed: false,
+  index: 0
+},
+]
+const arrSort = (arr) => {
+  return arr.sort((a,b) => a.index - b.index)
+}
 
-function component() {
-    const element = document.createElement('div');
+//function component() {
+    const list = document.querySelector('ul')
+    let sortedTodos = arrSort(todos)
+    if(sortedTodos.length > 0){
+
+      sortedTodos.forEach(el => {
+        const todo = document.createElement('div')
+        todo.setAttribute('class', "todoItem")
+        const titleCheckbox = document.createElement('div')
+        if(el.completed){
+          todo.classList.add("completed")
+        } else {
+          todo.classList.add("uncomplete")
+          const checkbox = document.createElement('input')
+          checkbox.type = "checkbox"
+          titleCheckbox.appendChild(checkbox)
+
+        }
+        const title = document.createElement('p')
+        title.classList.add("title")
+        title.innerText = el.description
+        const icon = document.createElement('i')
+        icon.classList.add("fa", "fa-ellipsis-v")
+        titleCheckbox.appendChild(title)
+        todo.appendChild(titleCheckbox)
+        todo.appendChild(icon)
+        list.appendChild(todo)
+      })
+    }
+
   
-      // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   
-    return element;
-  }
+  //  return list;
+  //}
   
-  document.body.appendChild(component());
+ // document.body.appendChild(component());
