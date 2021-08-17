4663c4414a779fc57b78e7210d6c542cf1b9981a
themonster2015 <quynhyen.vothi@gmail.com>
2021-08-17 16:01:28 +0200

    fixed linters

--- src/index.js
+++ src/index.js
@@ -1,57 +1,52 @@
-import './style.css'
-let todos = [{
-  description: "wash the dish",
+import './style.css';
+
+const todos = [{
+  description: 'wash the dish',
   completed: false,
-  index: 1
+  index: 1,
 },
 {
-  description: "complete the to do list project",
+  description: 'complete the to do list project',
   completed: false,
-  index: 2
+  index: 2,
 },
 {
-  description: "feed the dog",
+  description: 'feed the dog',
   completed: false,
-  index: 0
+  index: 0,
 },
-]
-const arrSort = (arr) => {
-  return arr.sort((a,b) => a.index - b.index)
+];
+const arrSort = (arr) => arr.sort((a, b) => a.index - b.index);
+
+// function component() {
+const list = document.querySelector('ul');
+const sortedTodos = arrSort(todos);
+if (sortedTodos.length > 0) {
+  sortedTodos.forEach((el) => {
+    const todo = document.createElement('div');
+    todo.setAttribute('class', 'todoItem');
+    const titleCheckbox = document.createElement('div');
+    if (el.completed) {
+      todo.classList.add('completed');
+    } else {
+      todo.classList.add('uncomplete');
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      titleCheckbox.appendChild(checkbox);
+    }
+    const title = document.createElement('p');
+    title.classList.add('title');
+    title.innerText = el.description;
+    const icon = document.createElement('i');
+    icon.classList.add('fa', 'fa-ellipsis-v');
+    titleCheckbox.appendChild(title);
+    todo.appendChild(titleCheckbox);
+    todo.appendChild(icon);
+    list.appendChild(todo);
+  });
 }
 
-//function component() {
-    const list = document.querySelector('ul')
-    let sortedTodos = arrSort(todos)
-    if(sortedTodos.length > 0){
+//  return list;
+// }
 
-      sortedTodos.forEach(el => {
-        const todo = document.createElement('div')
-        todo.setAttribute('class', "todoItem")
-        const titleCheckbox = document.createElement('div')
-        if(el.completed){
-          todo.classList.add("completed")
-        } else {
-          todo.classList.add("uncomplete")
-          const checkbox = document.createElement('input')
-          checkbox.type = "checkbox"
-          titleCheckbox.appendChild(checkbox)
-
-        }
-        const title = document.createElement('p')
-        title.classList.add("title")
-        title.innerText = el.description
-        const icon = document.createElement('i')
-        icon.classList.add("fa", "fa-ellipsis-v")
-        titleCheckbox.appendChild(title)
-        todo.appendChild(titleCheckbox)
-        todo.appendChild(icon)
-        list.appendChild(todo)
-      })
-    }
-
-  
-  
-  //  return list;
-  //}
-  
- // document.body.appendChild(component());
+// document.body.appendChild(component());
