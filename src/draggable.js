28ee50441aeeec959a216bad022a34329b2fdb71
themonster2015 <quynhyen.vothi@gmail.com>
2021-08-26 16:16:22 +0200

    added tests for clearCompleted

--- src/draggable.js
+++ src/draggable.js
@@ -15,7 +15,6 @@ export const dragstop = (e) => {
 export const drop = (sortedTodos, target, source) => {
   if (sortedTodos[target - 1]) {
     const temp = sortedTodos[target - 1].index;
-    console.log(target - 1);
     sortedTodos[target - 1].index = sortedTodos[source - 1].index;
     sortedTodos[source - 1].index = temp;
 
